6602f20faeecbb49bf0a8566ce787a20d4e948f2
Igor Osipov <ighor.osipov.95@mail.ru>
2024-05-24 12:56:23 +0300

    Создание интерфейса для работы с API с помощью Retrofit2.

---
+++ app/src/main/java/com/ighorosipov/testandroid/data/repository/CoinApi.kt
@@ -0,0 +1,16 @@
+package com.ighorosipov.testandroid.data.repository
+
+import com.ighorosipov.testandroid.data.remote.dto.CoinDetailDto
+import com.ighorosipov.testandroid.data.remote.dto.CoinDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CoinApi {
+
+    @GET("/v1/coins")
+    suspend fun getCoins(): List<CoinDto>
+
+    @GET("/v1/coins/{coinId}")
+    suspend fun getCoinById(@Path("coinId") coinId: String): CoinDetailDto
+
+}
